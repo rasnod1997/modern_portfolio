ea80ce777d650cb423c5a4bf775a4ec9fd84d923
Rasnod1997 <ryanaichouch@yahoo.fr>
2020-05-09 16:48:30 +0200

    Menu

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,13 +1,11 @@
 // Select DOM Items
-
 const menuBtn = document.querySelector(".menu-btn");
 const menu = document.querySelector(".menu");
 const menuNav = document.querySelector(".menu-nav");
 const menuBranding = document.querySelector(".menu-branding");
-
 const navItems = document.querySelectorAll(".nav-item");
 
-// Set inital state of Menu
+// Set Initial State Of Menu
 let showMenu = false;
 
 menuBtn.addEventListener("click", toggleMenu);
@@ -15,7 +13,7 @@ menuBtn.addEventListener("click", toggleMenu);
 function toggleMenu() {
   if (!showMenu) {
     menuBtn.classList.add("close");
-    menu.classList.add("close");
+    menu.classList.add("show");
     menuNav.classList.add("show");
     menuBranding.classList.add("show");
     navItems.forEach((item) => item.classList.add("show"));
